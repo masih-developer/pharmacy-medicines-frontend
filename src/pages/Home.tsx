8a909b9b7a460b5daa6e0b7d62692b15b622603d
masih abedini <masih7816@gmail.com>
2024-04-23 20:53:04 +0330

    🎨 (pages/Home.tsx): add MedicinesTable component into Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import MedicinesTable from "@/features/medicines/MedicinesTable";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main className="p-3">
+      <MedicinesTable />
+    </main>
+  );
 };
 
 export default HomePage;
