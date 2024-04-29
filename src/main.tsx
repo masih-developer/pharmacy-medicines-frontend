fe802b514823f7cecee1b0176cdff4f235db0171
masih abedini <masih7816@gmail.com>
2024-04-29 20:25:18 +0330

    ✨ (main.jsx): config tanstack-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import App from "./App.tsx";
 import "./assets/css/fonts.css";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
