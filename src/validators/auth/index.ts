485a262bc5702871467647738d8b03bb1f045c88
masih abedini <masih7816@gmail.com>
2024-04-30 23:21:20 +0330

    🦺 (validators/auth): create register and login schema

---
+++ src/validators/auth/index.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+const registerSchema = z.object({
+  firstname: z
+    .string()
+    .min(5, "اسم کوچک می بایست حداقل 5 کاراکتر باشد.")
+    .max(20, "اسم کوچک می بایست حداکثر 20 کاراکتر باشد."),
+  lastname: z
+    .string()
+    .min(5, "فامیلی می بایست حداقل 5 کاراکتر باشد.")
+    .max(20, "فامیلی می بایست حداکثر 20 کاراکتر باشد."),
+  username: z
+    .string()
+    .min(4, "نام کاربری می بایست حداقل 5 کاراکتر باشد.")
+    .max(20, "نام کاربری می بایست حداکثر 20 کاراکتر باشد.")
+    .regex(
+      /^[a-zA-Z]\w+$/,
+      "نام کاربری می بایست با حروف انگلیسی شروع شود و مجاز به استفاده اعداد و آندرلاین می باشید."
+    ),
+  email: z.string().email("ایمیل وارد شده معتبر نمی باشد."),
+  password: z
+    .string()
+    .min(6, "رمز عبور می بایست حداقل 6 کاراکتر باشد.")
+    .max(20, "رمز عبور می بایست حداکثر 20 کاراکتر باشد."),
+});
+
+const loginSchema = z.object({
+  email: registerSchema.shape.email,
+  password: registerSchema.shape.password,
+});
+
+export { registerSchema, loginSchema };
