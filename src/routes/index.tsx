dcb4a0c2aadf1ff6ab2e51538a724bf3e3cc34a6
masih abedini <masih7816@gmail.com>
2024-04-30 18:30:31 +0330

    🎨 (routes/index): add some routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,19 @@
+import OwnerLayout from "@/layouts/OwnerLayout";
 import HomePage from "@/pages/Home";
+import LoginPage from "@/pages/Login";
 import NotFoundPage from "@/pages/NotFound";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/" element={<OwnerLayout />}>
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<HomePage />} />
+      </Route>
+
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
