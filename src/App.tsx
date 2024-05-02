b79ca59781c41db666bad6595644a74dd3d797fd
masih abedini <masih7816@gmail.com>
2024-05-02 19:40:07 +0330

    🎨 (App.tsx): configure Toaster from react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { DirectionProvider } from "@radix-ui/react-direction";
 import { ThemeProvider } from "./context/ThemeProvider";
 import AppRoutes from "./routes";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="system" storageKey="theme">
       <DirectionProvider dir="rtl">
         <AppRoutes />
+        <Toaster
+          toastOptions={{
+            className: "bg-primary text-background max-w-[400px]",
+          }}
+        />
       </DirectionProvider>
     </ThemeProvider>
   );
