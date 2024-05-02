b140a8ef547b3209029d3c9bdb5f7696e3e3d7e7
masih abedini <masih7816@gmail.com>
2024-05-02 15:26:13 +0330

    🛂 (services/index.ts): add axios interceptors for handling 401 requests

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,4 +6,31 @@ const mainRequest = axios.create({
   withCredentials: true,
 });
 
+mainRequest.interceptors.request.use(
+  (res) => res,
+  (error) => Promise.reject(error)
+);
+
+mainRequest.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalConfig = error.config;
+
+    if (error.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/user/refresh`,
+          { withCredentials: true }
+        );
+        if (data) {
+          mainRequest(originalConfig);
+        }
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+  }
+);
+
 export default mainRequest;
