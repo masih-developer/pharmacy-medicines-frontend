fcf3f7f4e5a736b1def5f44d2be8e69e92764d13
masih abedini <masih7816@gmail.com>
2024-05-06 23:47:15 +0330

    ♻️ (validators/medicines): refactor validations

--- src/validators/medicines/index.ts
+++ src/validators/medicines/index.ts
@@ -4,10 +4,12 @@ export const medicineValidationSchema = z.object({
   name: z.string().min(1, "نام محصول می بایست حداقل 1 کاراکتر باشد."),
   expire: z
     .date({ message: "تاریخ وارد شده نا معبتر می باشد." })
-    .min(new Date(), "حداقل تاریخ انقضا فردا است.")
+    .min(new Date(), "حداقل تاریخ انقضا امروز است.")
     .transform((val) => new Date(val).toISOString()),
-  code: z.number(),
-  quantity: z.number({ message: "موجودی محصول می بایست عددی معتبر باشد." }),
-  price: z.number().min(0, "قیمت وارد شده معتبر نمی باشد."),
+  code: z.number({ message: "کد کالا می بایست یک مقدار عددی باشد!" }),
+  quantity: z.number({ message: "موجودی کالا می بایست یک مقدار عددی باشد!" }),
+  price: z
+    .number({ message: "قیمت کالا می بایست یک مقدار عددی باشد!" })
+    .min(0, "قیمت وارد شده معتبر نمی باشد."),
   type: z.string().min(1, "نوع محصول می بایست حداقل 1 کاراکتر باشد."),
 });
