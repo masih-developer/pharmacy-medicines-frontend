73f55e2d7c2171fb7044ca5bb8440d4196840e9d
masih abedini <masih7816@gmail.com>
2024-05-06 23:47:43 +0330

    ✨ (components/ui/date-picker): create DatePicker component

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,26 @@
+import ReactMultiDatePicker, {
+  CalendarProps,
+  DatePickerProps,
+} from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import { cn } from "@/lib/utils";
+
+const DatePicker: React.FC<
+  Omit<CalendarProps, "onChange"> & DatePickerProps
+> = ({ inputClass, ...rest }) => {
+  return (
+    <ReactMultiDatePicker
+      calendar={persian}
+      locale={persian_fa}
+      calendarPosition="bottom-right"
+      {...rest}
+      inputClass={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        inputClass
+      )}
+    />
+  );
+};
+
+export default DatePicker;
