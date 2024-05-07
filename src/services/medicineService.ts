459b544a687e18de884533a3a8913e7edcf7de72
masih abedini <masih7816@gmail.com>
2024-05-07 22:30:37 +0330

    👽️ (services/medicine): add "updateMedicineApi" function for update medicine

--- src/services/medicineService.ts
+++ src/services/medicineService.ts
@@ -1,7 +1,24 @@
 import { MedicinesApiParamsType } from "@/features/medicines/index.types";
 import mainRequest from ".";
+import { z } from "zod";
+import { medicineValidationSchema } from "@/validators/medicines";
 
 export const getMedicinesApi = async (params: MedicinesApiParamsType) => {
   const { data } = await mainRequest.get("/medicines", { params });
+  return data;
+};
+
+type updateMedicineApiType = {
+  id: string;
+  medicine: z.infer<typeof medicineValidationSchema>;
+};
+
+export const updateMedicineApi = async ({
+  id,
+  medicine,
+}: updateMedicineApiType) => {
+  const { data } = await mainRequest.put(`/medicines/${id}`, {
+    ...medicine,
+  });
   return data;
 };
