3efbc6ae08d4bd9a4776dec8d2fbcb95c1c2be58
masih abedini <masih7816@gmail.com>
2024-05-07 16:53:01 +0330

    🦺 (validators/medicines): refactor validate expire date using ISO formating

--- src/validators/medicines/index.ts
+++ src/validators/medicines/index.ts
@@ -1,11 +1,22 @@
 import { z } from "zod";
 
+// Custom validator for ISO date format
+const isoDate = z
+  .string()
+  .regex(
+    /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/,
+    "فرمت تاریخ وارد شده می بایست به فرمت  ISO باشد."
+  );
+
+// Custom validator for minimum date
+const minDate = z
+  .date()
+  .min(new Date(), "حداقل تاریخ انقضا امروز است.")
+  .transform((val) => new Date(val).toISOString());
+
 export const medicineValidationSchema = z.object({
   name: z.string().min(1, "نام محصول می بایست حداقل 1 کاراکتر باشد."),
-  expire: z
-    .date({ message: "تاریخ وارد شده نا معبتر می باشد." })
-    .min(new Date(), "حداقل تاریخ انقضا امروز است.")
-    .transform((val) => new Date(val).toISOString()),
+  expire: z.union([minDate, isoDate]),
   code: z.number({ message: "کد کالا می بایست یک مقدار عددی باشد!" }),
   quantity: z.number({ message: "موجودی کالا می بایست یک مقدار عددی باشد!" }),
   price: z
